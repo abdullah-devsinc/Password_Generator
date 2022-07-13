0b060efedc87da3316294dc83d4e5a545db29895
Muhammad Abdullah <108686474+abdullah-devsinc@users.noreply.github.com>
2022-07-13 19:49:10 +0500

    Update RandomPassword.rb

--- RandomPassword.rb
+++ RandomPassword.rb
@@ -2,5 +2,6 @@ def gen_password(p_lenght = 6)
   word_collection = ('a'...'z').to_a + ('A'...'Z').to_a + (0...9).to_a + ['!', '@', '#', '$', '$', '%', '^', '&', '>']
   word_collection.sample(p_lenght).join
 end
-
-print gen_password(10)
+print 'Enter the length : '
+passwordlength = gets.chomps.to_i
+print gen_password(passwordlength)
